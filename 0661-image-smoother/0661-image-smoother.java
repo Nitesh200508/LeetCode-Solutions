7a0b26e82980d7ca64ffba7eaf84f1059b58f964
Nitesh200508 <pniteshk4@gmail.com>
2026-06-24 14:45:19 +0530

    Time: 7 ms (30.6%), Space: 47.9 MB (50.14%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.java
+++ 0661-image-smoother/0661-image-smoother.java
@@ -13,8 +13,8 @@ class Solution {
         int[][] ans=new int[n][m];
         int sum=0;
         int avg=0;
-        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dy = {-1,  0,  1,-1, 1,-1, 0, 1};
+        int [] dx = {1,1,1,0,-1,-1,-1,0};
+        int [] dy = {1,0,-1,-1,-1,0,1,1};
 
         for(int i = 0;i < n;i++){
             for(int j = 0;j < m;j++){
