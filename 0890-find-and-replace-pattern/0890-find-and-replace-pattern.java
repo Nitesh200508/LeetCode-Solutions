ec70e2fe54fbef2050214b53fd80a34bb122d92e
Nitesh200508 <pniteshk4@gmail.com>
2026-07-15 11:02:54 +0530

    Time: 0 ms (100%), Space: 43.6 MB (80.17%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,17 +1,9 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         int n = s.length();
-        for(int i = 0;i < n-1;i++){
-            for(int j = i+1;j < n;j++){
-
-                if(s.charAt(i) == s.charAt(j) && t.charAt(i) != t.charAt(j)){
-                    
-                    return false;
-                }
-                if(s.charAt(i) != s.charAt(j) && t.charAt(i) == t.charAt(j)){
-                    
-                    return false;
-                }
+        for(int i = 0;i < n;i++){
+            if(s.indexOf(s.charAt(i))!=t.indexOf(t.charAt(i))){
+                return false;
             }
         }
         return true;
